02d55799f3c14148d3b49805b63608d69c95d1bc
aeholt97 <aeholt97@users.noreply.github.com>
2021-05-29 00:16:41 +0200

    test

--- jupyter one-click Github.bat
+++
@@ -1,9 +0,0 @@
-color 37
-title Jupyter Notebook Kernel - Python 3.8
-set root=C:\Users\aehga\miniconda3
-cls
-call %root%/Scripts/activate.bat %root%
-cd C:\Users\aehga\Documents\GitHub_Noter\aeholt97.github.io
-
-call conda activate py38
-call jupyter notebook
